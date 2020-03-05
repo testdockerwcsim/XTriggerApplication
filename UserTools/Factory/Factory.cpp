12b0ac69962a0fd0b82dec2c20ccb2364dc32ea9
Lukas Koch <lukas.koch@stfc.ac.uk>
2020-03-05 16:22:57 +0000

    Reimplement NHits CPU algorithm.
    New algorithm only loops over digits in SubSample twice.
    
    Changed coding style to follow conventions.

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -8,7 +8,7 @@ if (tool=="DummyTool") ret=new DummyTool;
 
 if (tool=="TriggerOutput") ret=new TriggerOutput;
 if (tool=="WCSimASCIReader") ret=new WCSimASCIReader;
-if (tool=="nhits") ret=new nhits;
+if (tool=="NHits") ret=new NHits;
 if (tool=="test_vertices") ret=new test_vertices;
 if (tool=="WCSimReader") ret=new WCSimReader;
 if (tool=="DataOut") ret=new DataOut;
