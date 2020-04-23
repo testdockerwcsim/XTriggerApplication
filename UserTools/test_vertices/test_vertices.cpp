22d7f7dc24e896261eac3ba12d58b56c2ee84c54
federiconova <federico.nova@stfc.ac.uk>
2020-04-23 18:43:05 +0100

    f_ -> m_

--- UserTools/test_vertices/test_vertices.cpp
+++ UserTools/test_vertices/test_vertices.cpp
@@ -23,47 +23,47 @@ bool test_vertices::Initialise(std::string configfile, DataModel &data){
   m_variables.Get("DetectorFile",DetectorFile);
   m_variables.Get("ParameterFile",ParameterFile);
   
-  m_variables.Get("distance_between_vertices",        f_distance_between_vertices);
-  m_variables.Get("wall_like_distance",   f_wall_like_distance);
-  m_variables.Get("water_like_threshold_number_of_pmts",   f_water_like_threshold_number_of_pmts);
-  m_variables.Get("wall_like_threshold_number_of_pmts",   f_wall_like_threshold_number_of_pmts);
-  m_variables.Get("coalesce_time",   f_coalesce_time);
-  m_variables.Get("trigger_gate_up",   f_trigger_gate_up);
-  m_variables.Get("trigger_gate_down",   f_trigger_gate_down);
-  m_variables.Get("output_txt",   f_output_txt);
-  m_variables.Get("correct_mode",   f_correct_mode);
-  m_variables.Get("n_direction_bins_theta",   f_n_direction_bins_theta);
-  m_variables.Get("cylindrical_grid",   f_cylindrical_grid);
-  m_variables.Get("costheta_cone_cut",   f_costheta_cone_cut);
-  m_variables.Get("select_based_on_cone",   f_select_based_on_cone);
-  m_variables.Get("write_output_mode",   f_write_output_mode);
-  m_variables.Get("trigger_threshold_adjust_for_noise",                   f_trigger_threshold_adjust_for_noise);
-  m_variables.Get("max_n_hits_per_job",   f_max_n_hits_per_job);
-  m_variables.Get("num_blocks_y",   f_num_blocks_y);
-  m_variables.Get("num_threads_per_block_y",   f_num_threads_per_block_y);
-  m_variables.Get("num_threads_per_block_x",   f_num_threads_per_block_x);
+  m_variables.Get("distance_between_vertices",        m_distance_between_vertices);
+  m_variables.Get("wall_like_distance",   m_wall_like_distance);
+  m_variables.Get("water_like_threshold_number_of_pmts",   m_water_like_threshold_number_of_pmts);
+  m_variables.Get("wall_like_threshold_number_of_pmts",   m_wall_like_threshold_number_of_pmts);
+  m_variables.Get("coalesce_time",   m_coalesce_time);
+  m_variables.Get("trigger_gate_up",   m_trigger_gate_up);
+  m_variables.Get("trigger_gate_down",   m_trigger_gate_down);
+  m_variables.Get("output_txt",   m_output_txt);
+  m_variables.Get("correct_mode",   m_correct_mode);
+  m_variables.Get("n_direction_bins_theta",   m_n_direction_bins_theta);
+  m_variables.Get("cylindrical_grid",   m_cylindrical_grid);
+  m_variables.Get("costheta_cone_cut",   m_costheta_cone_cut);
+  m_variables.Get("select_based_on_cone",   m_select_based_on_cone);
+  m_variables.Get("write_output_mode",   m_write_output_mode);
+  m_variables.Get("trigger_threshold_adjust_for_noise",                   m_trigger_threshold_adjust_for_noise);
+  m_variables.Get("max_n_hits_per_job",   m_max_n_hits_per_job);
+  m_variables.Get("num_blocks_y",   m_num_blocks_y);
+  m_variables.Get("num_threads_per_block_y",   m_num_threads_per_block_y);
+  m_variables.Get("num_threads_per_block_x",   m_num_threads_per_block_x);
 
   printf(" DetectorFile %s \n ", DetectorFile.c_str());
   printf(" ParameterFile %s \n ",ParameterFile.c_str() );
-  printf(" f_distance_between_vertices %f \n ", f_distance_between_vertices);
-  printf(" f_wall_like_distance %f \n ", f_wall_like_distance);
-  printf(" f_water_like_threshold_number_of_pmts %f \n ", f_water_like_threshold_number_of_pmts);
-  printf(" f_wall_like_threshold_number_of_pmts %f \n ", f_wall_like_threshold_number_of_pmts);
-  printf(" f_coalesce_time %f \n ", f_coalesce_time);
-  printf(" f_trigger_gate_up %f \n ", f_trigger_gate_up);
-  printf(" f_trigger_gate_down %f \n ", f_trigger_gate_down);
-  printf(" f_output_txt %d \n ", f_output_txt);
-  printf(" f_correct_mode %d \n ", f_correct_mode);
-  printf(" f_n_direction_bins_theta %d \n ",   f_n_direction_bins_theta);
-  printf(" f_cylindrical_grid %d \n ",   f_cylindrical_grid);
-  printf(" f_costheta_cone_cut %f \n ",   f_costheta_cone_cut);
-  printf(" f_select_based_on_cone %d \n ",   f_select_based_on_cone);
-  printf(" f_write_output_mode %d \n ", f_write_output_mode);
-  printf(" f_max_n_hits_per_job %d \n ", f_max_n_hits_per_job);
-  printf(" f_trigger_threshold_adjust_for_noise %d \n ", f_trigger_threshold_adjust_for_noise);
-  printf(" f_num_blocks_y %d \n ",   f_num_blocks_y);
-  printf(" f_num_threads_per_block_y %d \n ",   f_num_threads_per_block_y);
-  printf(" f_num_threads_per_block_x %d \n ",   f_num_threads_per_block_x);
+  printf(" m_distance_between_vertices %f \n ", m_distance_between_vertices);
+  printf(" m_wall_like_distance %f \n ", m_wall_like_distance);
+  printf(" m_water_like_threshold_number_of_pmts %f \n ", m_water_like_threshold_number_of_pmts);
+  printf(" m_wall_like_threshold_number_of_pmts %f \n ", m_wall_like_threshold_number_of_pmts);
+  printf(" m_coalesce_time %f \n ", m_coalesce_time);
+  printf(" m_trigger_gate_up %f \n ", m_trigger_gate_up);
+  printf(" m_trigger_gate_down %f \n ", m_trigger_gate_down);
+  printf(" m_output_txt %d \n ", m_output_txt);
+  printf(" m_correct_mode %d \n ", m_correct_mode);
+  printf(" m_n_direction_bins_theta %d \n ",   m_n_direction_bins_theta);
+  printf(" m_cylindrical_grid %d \n ",   m_cylindrical_grid);
+  printf(" m_costheta_cone_cut %f \n ",   m_costheta_cone_cut);
+  printf(" m_select_based_on_cone %d \n ",   m_select_based_on_cone);
+  printf(" m_write_output_mode %d \n ", m_write_output_mode);
+  printf(" m_max_n_hits_per_job %d \n ", m_max_n_hits_per_job);
+  printf(" m_trigger_threshold_adjust_for_noise %d \n ", m_trigger_threshold_adjust_for_noise);
+  printf(" m_num_blocks_y %d \n ",   m_num_blocks_y);
+  printf(" m_num_threads_per_block_y %d \n ",   m_num_threads_per_block_y);
+  printf(" m_num_threads_per_block_x %d \n ",   m_num_threads_per_block_x);
 
 
   //  gpu_daq_initialize(PMTFile,DetectorFile,ParameterFile);
@@ -86,25 +86,25 @@ bool test_vertices::Initialise(std::string configfile, DataModel &data){
   
 
   GPU_daq::test_vertices_initialize_ToolDAQ(m_data->detector_length, m_data->detector_radius, m_data->pmt_radius, ParameterFile, tube_no, tube_x, tube_y, tube_z, m_data->IDPMTDarkRate*1000,
- f_distance_between_vertices,
- f_wall_like_distance,
- f_water_like_threshold_number_of_pmts,
- f_wall_like_threshold_number_of_pmts,
- f_coalesce_time,
- f_trigger_gate_up,
- f_trigger_gate_down,
- f_output_txt,
- f_correct_mode,
- f_n_direction_bins_theta,
- f_cylindrical_grid,
- f_costheta_cone_cut,
- f_select_based_on_cone,
- f_trigger_threshold_adjust_for_noise,
- f_max_n_hits_per_job,
- f_num_blocks_y,
- f_num_threads_per_block_y,
- f_num_threads_per_block_x,
- f_write_output_mode
+ m_distance_between_vertices,
+ m_wall_like_distance,
+ m_water_like_threshold_number_of_pmts,
+ m_wall_like_threshold_number_of_pmts,
+ m_coalesce_time,
+ m_trigger_gate_up,
+ m_trigger_gate_down,
+ m_output_txt,
+ m_correct_mode,
+ m_n_direction_bins_theta,
+ m_cylindrical_grid,
+ m_costheta_cone_cut,
+ m_select_based_on_cone,
+ m_trigger_threshold_adjust_for_noise,
+ m_max_n_hits_per_job,
+ m_num_blocks_y,
+ m_num_threads_per_block_y,
+ m_num_threads_per_block_x,
+ m_write_output_mode
 );
 #endif
 
@@ -145,8 +145,8 @@ bool test_vertices::Execute(){
     GPU_daq::test_vertices_execute(is->m_PMTid, m_time_int, &trigger_ns, &trigger_ts);
     for(int i=0; i<trigger_ns.size(); i++){
       m_data->IDTriggers.AddTrigger(kTriggerUndefined,
-				    trigger_ts[i] + f_trigger_gate_down, 
-				    trigger_ts[i] + f_trigger_gate_up,
+				    trigger_ts[i] + m_trigger_gate_down, 
+				    trigger_ts[i] + m_trigger_gate_up,
 				    trigger_ts[i],
 				    std::vector<float>(1, trigger_ns[i]));
 
--- UserTools/test_vertices/test_vertices.h
+++ UserTools/test_vertices/test_vertices.h
@@ -21,25 +21,25 @@ class test_vertices: public Tool {
 
  private:
 
-  float f_distance_between_vertices;
-  float f_wall_like_distance;
-  float f_water_like_threshold_number_of_pmts;
-  float f_wall_like_threshold_number_of_pmts;
-  float f_coalesce_time;
-  float f_trigger_gate_up;
-  float f_trigger_gate_down;
-  int f_output_txt;
-  int f_correct_mode;
-  int f_n_direction_bins_theta;
-  bool f_cylindrical_grid;
-  float f_costheta_cone_cut;
-  bool f_select_based_on_cone;
-  bool  f_trigger_threshold_adjust_for_noise;
-  int f_max_n_hits_per_job;
-  int f_num_blocks_y;
-  int f_num_threads_per_block_y;
-  int f_num_threads_per_block_x;
-  int f_write_output_mode;
+  float m_distance_between_vertices;
+  float m_wall_like_distance;
+  float m_water_like_threshold_number_of_pmts;
+  float m_wall_like_threshold_number_of_pmts;
+  float m_coalesce_time;
+  float m_trigger_gate_up;
+  float m_trigger_gate_down;
+  int m_output_txt;
+  int m_correct_mode;
+  int m_n_direction_bins_theta;
+  bool m_cylindrical_grid;
+  float m_costheta_cone_cut;
+  bool m_select_based_on_cone;
+  bool  m_trigger_threshold_adjust_for_noise;
+  int m_max_n_hits_per_job;
+  int m_num_blocks_y;
+  int m_num_threads_per_block_y;
+  int m_num_threads_per_block_x;
+  int m_write_output_mode;
 
 
 
