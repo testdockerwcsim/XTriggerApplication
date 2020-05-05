b3f0ec1de73cbf1094a6b9cdb702143dc0d4589a
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2020-05-05 14:33:25 +0100

    Add doxygen for log messages

--- UserTools/EnergeticBONSAI/EnergeticBONSAI.h
+++ UserTools/EnergeticBONSAI/EnergeticBONSAI.h
@@ -56,17 +56,21 @@ class EnergeticBONSAI: public Tool {
   /// Image filename to save the histogram to, if required
   std::string m_stopwatch_file;
 
+  /// Verbosity level, as defined in tool parameter file
   int m_verbose;
 
+  /// For easy formatting of Log messages
   std::stringstream m_ss;
 
+  /// Print the current value of the streamer at the set log level,
+  ///  then clear the streamer
   void StreamToLog(int level) {
     Log(m_ss.str(), level, m_verbose);
     m_ss.str("");
   }
 
+  /// Log level enumerations
   enum LogLevel {FATAL=-1, ERROR=0, WARN=1, INFO=2, DEBUG1=3, DEBUG2=4, DEBUG3=5};
-
 
 };
 
