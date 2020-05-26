0d9dc5faa1996a45574da89748a8a8645516faf7
federiconova <federico.nova@stfc.ac.uk>
2020-05-26 11:26:06 +0100

    ifdef BONSAI

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -12,7 +12,9 @@ if (tool=="NHits") ret=new NHits;
 if (tool=="test_vertices") ret=new test_vertices;
 if (tool=="WCSimReader") ret=new WCSimReader;
 if (tool=="DataOut") ret=new DataOut;
+#ifdef BONSAIEXISTS
 if (tool=="BONSAI") ret=new BONSAI;
+#endif
 if (tool=="ReconDataOut") ret=new ReconDataOut;
 if (tool=="dimfit") ret=new dimfit;
 if (tool=="ReconRandomiser") ret=new ReconRandomiser;
