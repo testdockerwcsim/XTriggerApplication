0b9d4b4643641ac0a5a8b2cc2fc165ae2d2a75c7
federiconova <federico.nova@stfc.ac.uk>
2020-08-06 09:31:40 +0100

    trigger output

--- UserTools/TriggerOutput/TriggerOutput.cpp
+++ UserTools/TriggerOutput/TriggerOutput.cpp
@@ -8,33 +8,118 @@ bool TriggerOutput::Initialise(std::string configfile, DataModel &data){
   if(configfile!="")  m_variables.Initialise(configfile);
   //m_variables.Print();
 
+  m_verbose = 0;
+  m_variables.Get("verbose", m_verbose);
+  m_variables.Get("trigger_od",                   m_trigger_OD);
+
+  //Setup and start the stopwatch
+  bool use_stopwatch = false;
+  m_variables.Get("use_stopwatch", use_stopwatch);
+  m_stopwatch = use_stopwatch ? new util::Stopwatch("nhits") : 0;
+
+  m_stopwatch_file = "";
+  m_variables.Get("stopwatch_file", m_stopwatch_file);
+
+  if(m_stopwatch) m_stopwatch->Start();
+
   m_data= &data;
+
+  //open the output file
+  Log("DEBUG: DataOut::Initialise opening output file...", DEBUG2, m_verbose);
+  if(! m_variables.Get("outfilename", m_output_filename)) {
+    Log("ERROR: outfilename configuration not found. Cancelling initialisation", ERROR, m_verbose);
+    return false;
+  }
+  m_output_file = new TFile(m_output_filename.c_str(), "RECREATE");
+
+
+  //setup the out event tree
+  Log("DEBUG: DataOut::Initialise setting up output event tree...", DEBUG2, m_verbose);
+  // Nevents unique event objects
+  m_event_tree = new TTree("triggers","triggers Tree");
+  m_event_tree->Branch("type", &the_type, "type/I");
+  m_event_tree->Branch("readout_start_time", &the_readout_start_time, "readout_start_time/F");
+  m_event_tree->Branch("readout_end_time", &the_readout_end_time, "readout_end_time/F");
+  m_event_tree->Branch("mask_start_time", &the_mask_start_time, "mask_start_time/F");
+  m_event_tree->Branch("mask_end_time", &the_mask_end_time, "mask_end_time/F");
+  m_event_tree->Branch("trigger_time", &the_trigger_time, "trigger_time/F");
+
+  //fill the output event-independent trees
+  Log("DEBUG: DataOut::Initialise filling event-independent trees...", DEBUG2, m_verbose);
+
+  if(m_stopwatch) Log(m_stopwatch->Result("Initialise"), INFO, m_verbose);
 
   return true;
 }
 
 
 bool TriggerOutput::Execute(){
+  if(m_stopwatch) m_stopwatch->Start();
 
-  std::string outfile;
-  m_variables.Get("outfile",outfile);
-  
-  std::ofstream out (outfile.c_str());
-  if (out.is_open())
-    {
-      out<<m_data->triggeroutput;
-      out.close();
-    }
-  else {
-    std::cout << "Unable to open file";
-    return false;  
+
+  TriggerInfo * triggers = m_trigger_OD ? &(m_data->ODTriggers) : &(m_data->IDTriggers);
+
+  /// The number of triggers
+  unsigned int num_triggers = triggers->m_num_triggers;
+  /// The type of Trigger
+  std::vector<TriggerType_t> type = triggers->m_type;
+  /// The starting time of the trigger window
+  std::vector<TimeDelta> readout_start_time = triggers->m_readout_start_time;
+  /// The ending time of the trigger window
+  std::vector<TimeDelta> readout_end_time = triggers->m_readout_end_time;
+  /// The starting time of the hit mask
+  std::vector<TimeDelta> mask_start_time = triggers->m_mask_start_time;
+  /// The ending time of the hit mask
+  std::vector<TimeDelta> mask_end_time = triggers->m_mask_end_time;
+  /// The actual time of the trigger
+  std::vector<TimeDelta> trigger_time = triggers->m_trigger_time;
+  /// Additional information, specific to the trigger
+  std::vector<std::vector<float> > info = triggers->m_info;
+
+  m_ss << " TriggerOutput: Number of triggers " << num_triggers;
+  StreamToLog(INFO);
+  for(int i=0; i<num_triggers; i++){
+    the_type = type[i];
+    the_readout_start_time = readout_start_time[i];
+    the_readout_end_time = readout_end_time[i];
+    the_mask_start_time = mask_start_time[i];
+    the_mask_end_time = mask_end_time[i];
+    the_trigger_time = trigger_time[i];
+    m_ss << " TriggerOutput: trigger " << i << " type " << WCSimEnumerations::EnumAsString(the_type) << " readout_start_time " << the_readout_start_time << " readout_end_time " << the_readout_end_time << " mask_start_time " << the_mask_start_time << " mask_end_time " << the_mask_end_time << " trigger_time " << the_trigger_time ;
+    for(unsigned int ii = 0; ii < info[i].size(); ii++)
+      m_ss << " info " << ii << " = " << info[i][ii];
+
+    StreamToLog(INFO);
+
+    m_event_tree->Fill();
   }
-  
+
+  if(m_stopwatch) m_stopwatch->Stop();
+
   return true;
 }
 
 
 bool TriggerOutput::Finalise(){
+
+  if(m_stopwatch) {
+    Log(m_stopwatch->Result("Execute", m_stopwatch_file), INFO, m_verbose);
+    m_stopwatch->Start();
+  }
+
+  //multiple TFiles may be open. Ensure we save to the correct one
+  m_output_file->cd(TString::Format("%s:/", m_output_filename.c_str()));
+  m_event_tree->Write();
+
+  delete m_event_tree;
+
+  m_output_file->Close();
+  delete m_output_file;
+
+  if(m_stopwatch) {
+    Log(m_stopwatch->Result("Finalise"), INFO, m_verbose);
+    delete m_stopwatch;
+  }
 
   return true;
 }
--- UserTools/TriggerOutput/TriggerOutput.h
+++ UserTools/TriggerOutput/TriggerOutput.h
@@ -7,6 +7,11 @@
 
 #include "Tool.h"
 
+#include "TFile.h"
+#include "TTree.h"
+
+#include "Stopwatch.h"
+
 class TriggerOutput: public Tool {
 
 
@@ -21,8 +26,43 @@ class TriggerOutput: public Tool {
  private:
 
 
+  /// Trigger on OD digits, rather than ID digits?
+  bool m_trigger_OD;
 
+  /// Output ROOT filename that this tool RECREATE's
+  std::string m_output_filename;
+  /// Output ROOT file
+  TFile * m_output_file;
+  /// Tree contain WCSimRootEvent(s), and the original WCSim filename / event number
+  TTree * m_event_tree;
+  /// The type of Trigger
+  TriggerType_t the_type;
+  /// The starting time of the trigger window
+  TimeDelta the_readout_start_time;
+  /// The ending time of the trigger window
+  TimeDelta the_readout_end_time;
+  /// The starting time of the hit mask
+  TimeDelta the_mask_start_time;
+  /// The ending time of the hit mask
+  TimeDelta the_mask_end_time;
+  /// The actual time of the trigger
+  TimeDelta the_trigger_time;
 
+  /// The stopwatch, if we're using one
+  util::Stopwatch * m_stopwatch;
+  /// Image filename to save the histogram to, if required
+  std::string m_stopwatch_file;
+
+  int m_verbose;
+
+  std::stringstream m_ss;
+
+  void StreamToLog(int level) {
+    Log(m_ss.str(), level, m_verbose);
+    m_ss.str("");
+  }
+
+  enum LogLevel {FATAL=-1, ERROR=0, WARN=1, INFO=2, DEBUG1=3, DEBUG2=4, DEBUG3=5};
 
 };
 
